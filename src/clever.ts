da430d2dbb3fe6e913608657f14f6070779dce92
Jarom Loveridge <jaroml@gradecam.com>
2019-08-28 21:54:12 -0600

    Add support for retrieving application tokens.

--- src/clever.ts
+++ src/clever.ts
@@ -1,8 +1,10 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import { createClient, Config as ClientConfig, Client } from './api';
+import { ApplicationToken } from './api/schema';
 
 export function createClever(clientId: string, clientSecret: string = ''): Clever {
   const auth = {username: clientId, password: clientSecret};
+  const tokensUrl = 'https://clever.com/oauth/tokens';
   const clever = {
     getAccessToken: async ({code, redirectUri}: AccessTokenOpts): Promise<string> => {
       const data= {
@@ -11,11 +13,10 @@ export function createClever(clientId: string, clientSecret: string = ''): Cleve
         grant_type: 'authorization_code',
         redirect_uri: redirectUri,
       };
-      const url = 'https://clever.com/oauth/tokens';
       const requestConfig: AxiosRequestConfig = {
         auth, data,
         method: 'POST',
-        url,
+        url: tokensUrl,
       };
       return axios(requestConfig)
         .then((result: AxiosResponse<{access_token: string;}>) => result.data.access_token)
@@ -41,14 +42,23 @@ export function createClever(clientId: string, clientSecret: string = ''): Cleve
       if (opts.districtId) { params.push(`district_id=${opts.districtId}`); }
       return 'https://clever.com/oauth/authorize?' + params.join('&');
     },
+    listAppTokens: (params: ListTokensParams = {}) => {
+      if (!params.owner_type) { params.owner_type = 'district'; }
+      return axios({method: 'GET', auth, url: tokensUrl, params})
+        .then((resp: AxiosResponse<{data: ApplicationToken[]}>) => resp.data.data)
+      ;
+    },
     getClient: createClient,
   };
   return clever;
 }
 
+export default createClever;
+
 export interface Clever {
   getAccessToken(opts: AccessTokenOpts): Promise<string>;
   getAuthorizationUrl(opts: AuthUrlOpts): string;
+  listAppTokens(params: ListTokensParams): Promise<ApplicationToken[]>;
   getClient(opts: ClientConfig): Client;
 }
 
@@ -61,4 +71,11 @@ export interface AuthUrlOpts {
   districtId?: string;
   redirectUri?: string;
   state?: object | string;
+}
+
+export interface ListTokensParams {
+  /** id of the district to limit the responses */
+  district?: string;
+  /** according to Clever's documentation the only valid value is 'district' */
+  owner_type?: string;
 }
